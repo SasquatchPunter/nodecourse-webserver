0bf649291c13ff6d322706e2c5ae0cae9cdfd9d9
Jeremy Elliott <jodomoi64@yahoo.ca>
2020-12-08 17:44:32 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+console.log('Loading client side javascript...');
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#message1')
+const msg2 = document.querySelector('#message2')
+const unitButton = document.querySelector('#unitButton')
+
+const uvals = [['m', '\u00B0C'], ['f', '\u00B0F'], ['s', 'K']] // unit value pairs
+var uidx = 0 // represents uvals index
+unitButton.textContent = uvals[uidx][1]
+
+msg1.textContent, msg2.textContent = ''
+
+unitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    unitButton.textContent = uvals[uidx = (uidx + 1) % 3][1]
+})
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    msg1.textContent = 'Loading Data...'
+    msg2.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location + '&units=' + uvals[uidx][0]).then((res) => {
+        res.json().then(({ error, data }) => {
+            if (error) {
+                msg1.textContent = error
+                return console.log('There was an error finding weather data.')
+            }
+            const { temperature, feelslike, precipitation, pressure, humidity, description, units } = data
+            const { name, region, country } = data.location
+            const degChar = uvals[uidx][1]
+
+            msg1.textContent = name + ', ' + region + ', ' + country
+            msg2.textContent = 'It is ' + temperature + degChar + ' but feels like ' + feelslike + degChar + '.\n' +
+                'The weather is currently ' + description + '.'
+
+            console.log('Weather data retrieved and text data finished rendering.')
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,138 @@
+const path = require('path');
+const express = require('express');
+const request = require('request');
+const hbs = require('hbs');
+const { info } = require('console');
+const { forecast } = require(path.join(__dirname, '../../weather-app/utils/forecast'));
+const geocode = require(path.join(__dirname, '../../weather-app/utils/geocode'));
+
+// Define express config paths
+const publicDir = path.join(__dirname, '../public');
+const viewsDir = path.join(__dirname, '../templates/views');
+const partialsDir = path.join(__dirname, '../templates/partials');
+
+// Setup express and static directory to serve
+const app = express();
+app.use(express.static(publicDir));
+
+// Setup handlebars engine and paths
+app.set('view engine', 'hbs');
+app.set('views', viewsDir);
+hbs.registerPartials(partialsDir);
+
+// Const template objects
+const authName = 'Jeremy Elliott';
+
+// Home Page
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Main Page',
+        message: 'Find the root page here.',
+        authName
+    });
+});
+
+// About Page 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        message: 'Find info about me here.',
+        info: {
+            name: 'Jeremy Elliott',
+            location: 'Canmore, Alberta',
+            age: '30',
+            sex: 'YES',
+            catchphrase: '\'Fucking Hell!\''
+        },
+        authName
+    });
+});
+
+// Help Page
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'Find help info here.',
+        authName
+    });
+});
+
+// Practice for passing in query strings
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+
+    res.send({
+        queryData: req.query
+    });
+});
+
+// Weather Page
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(req.query.units, longitude, latitude, (error, data) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                data,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
+
+});
+
+const html = '<html><body><h1>This Is A Header</h1></body></html>';
+
+app.get('/forecast', (req, res) => {
+    // const url = 'http://api.weatherstack.com/current?access_key=6464bfe867a8b90e166764f2f7e759f6&query=Toronto';
+    // request({ url: url }, (error, response) => {
+    //     if (error) {
+    //         res.send('There was an error getting the forecast!');
+    //     } else {
+    //         res.send(JSON.parse(response.body));
+    //     }
+    // });
+    res.render('weather', {
+        title: 'Weather Page',
+        authName,
+        message: 'Search locations for up-to-the-minute weather data!'
+    })
+
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('error-404', {
+        title: 'Error 404',
+        message: 'Help article not found!',
+        authName
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('error-404', {
+        title: '404 Error',
+        message: 'Page not found!',
+        authName
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Port 3000 connected.');
+});
